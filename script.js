a39df038dd0b2c923f335e5b4b5d1c7a4fd5f682
sabrinalealfarias-droid <sabrina.leal.farias@escola.pr.gov.br>
2026-06-11 09:05:34 -0300

    botao

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+    const botao = document.querySelector("button")
+    botao.addEventListener('click' , curtir())
+
+function curtir(){
+    let curtidas = document.querySelector("span")
+
+    //botão do tamanho
+    const tamanho = document.getElementById("tamanho
+        tamanho.addEvetlistener("click, aumentarFonte)
+
+            ///função
+            function aumentarForte(){
+                fonte++;
+                document.body.style.fontSize
